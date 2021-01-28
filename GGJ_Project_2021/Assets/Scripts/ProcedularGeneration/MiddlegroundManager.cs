eafb2fac7109e7e644d0092527abf0c33040d1ba
DreamingJohnny <johnnydreaming@gmail.com>
2021-01-28 17:48:58 +0100

    Added a very basic middleground spawner

---
+++ GGJ_Project_2021/Assets/Scripts/ProcedularGeneration/MiddlegroundManager.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MiddlegroundManager: MonoBehaviour
+{
+    public GameObject middleGroundObject;
+
+    float timeUntilNextSpawn;
+    float timeSinceLastSpawn;
+    float minimumTimeUntilSpawn = 8;
+    float maximumTimeUntilSpawn = 16;
+
+    void Start()
+    {
+        Vector2 positionMiddleGroundObject = transform.position;
+
+        timeSinceLastSpawn = 0;
+
+        timeUntilNextSpawn = Random.Range(minimumTimeUntilSpawn, maximumTimeUntilSpawn);
+    }
+
+    void Update()
+    {
+        if (timeSinceLastSpawn >= timeUntilNextSpawn)
+        {
+            Instantiate(middleGroundObject, transform.position, Quaternion.identity);
+
+            timeSinceLastSpawn = 0;
+            timeUntilNextSpawn = Random.Range(minimumTimeUntilSpawn, maximumTimeUntilSpawn);
+        }
+        else
+        {
+            timeSinceLastSpawn += Time.deltaTime;
+        }
+    }
+}
