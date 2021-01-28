11f8276a8baf6a01d6d2b4f71ece001d974a5461
DreamingJohnny <johnnydreaming@gmail.com>
2021-01-28 11:29:52 +0100

    Fixed hitbox for obstacle & racoon and deathzone

--- GGJ_Project_2021/Assets/DestroyerZone.cs
+++ GGJ_Project_2021/Assets/DestroyerZone.cs
@@ -2,10 +2,8 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class PlayerMovement : MonoBehaviour
+public class DestroyerZone : MonoBehaviour
 {
-
-    public float playerSpeed;
     // Start is called before the first frame update
     void Start()
     {
@@ -15,6 +13,15 @@ public class PlayerMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.CompareTag("Player"))
+        {
+            Debug.Log("Game Over!");
+        }
     }
 }
+
--- GGJ_Project_2021/Assets/Scripts/GameManager.cs
+++ GGJ_Project_2021/Assets/Scripts/GameManager.cs
@@ -4,15 +4,17 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    // Start is called before the first frame update
+
+    public float constantMovement = 1;
+
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(Vector2.left * constantMovement * Time.deltaTime);    
     }
+
 }
