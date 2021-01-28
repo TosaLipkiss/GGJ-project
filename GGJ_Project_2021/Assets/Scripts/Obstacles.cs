190b9174e1eb5fd99b09c5ab65dc23701b18f967
DreamingJohnny <johnnydreaming@gmail.com>
2021-01-28 14:09:48 +0100

    Messed arround with streetlamps

--- GGJ_Project_2021/Assets/Scripts/Obstacles.cs
+++ GGJ_Project_2021/Assets/Scripts/Obstacles.cs
@@ -4,23 +4,12 @@ using UnityEngine;
 
 public class Obstacles : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 
     private void OnTriggerEnter2D(Collider2D collision)
     {
         if (collision.gameObject.tag == "DeathZone")
         {
-            Debug.Log("Destroys other than player!");
+            Debug.Log("Obstacle destroyed");
 
             Destroy(gameObject);
         }
