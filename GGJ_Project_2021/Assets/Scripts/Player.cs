8ebee653d779b21b326b769a87dbb9509d453e35
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-01-28 18:38:27 +0100

    animations and flip implemented on player

--- GGJ_Project_2021/Assets/Scripts/Player.cs
+++ GGJ_Project_2021/Assets/Scripts/Player.cs
@@ -4,28 +4,39 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    Animator animator;
     Rigidbody2D rb;
-    float directionX;
-    float moveSpeed = 5.0f;
+
+    float directionX = 0.0f;
+    float runSpeed = 5.0f;
     float jumpSpeed = 7.0f;
 
     bool isJumping = false;
+    bool facingRight;
 
     private void Start()
     {
+        facingRight = true;
+        animator = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
+        /*
         if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
         {
-            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            transform.Translate(Vector2.right * runSpeed * Time.deltaTime);
         }
         if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
         {
-            transform.Translate(-Vector2.right * moveSpeed * Time.deltaTime);
+            transform.Translate(-Vector2.right * runSpeed * Time.deltaTime);
         }
+        */
+        directionX = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+        animator.SetFloat("Speed", Mathf.Abs(directionX));
+        Flip(directionX);
         Jump();
     }
 
@@ -39,6 +50,7 @@ public class Player : MonoBehaviour
         if (Input.GetButtonDown("Jump") && isJumping == false)
         {
             rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            animator.SetBool("Jump", true);
             isJumping = true;
         }
     }
@@ -48,7 +60,20 @@ public class Player : MonoBehaviour
         if(collision.gameObject.tag == "Ground")
         {
             Debug.Log("Collides with deathzone");
+            animator.SetBool("Jump", false);
             isJumping = false;
+        }
+    }
+
+    void Flip(float directionX)
+    {
+        if(directionX > 0 && !facingRight || directionX < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+
+            Vector3 playerScale = transform.localScale;
+            playerScale.x *= -1;
+            transform.localScale = playerScale;
         }
     }
 }
