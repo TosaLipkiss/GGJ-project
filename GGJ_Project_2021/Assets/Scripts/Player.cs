e1ef8d971a058e33ce40a843c4328b2bc6e229c9
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-01-28 14:06:05 +0100

    Player script

---
+++ GGJ_Project_2021/Assets/Scripts/Player.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    Rigidbody2D rb;
+    float directionX;
+    float moveSpeed = 1.5f;
+    float jumpSpeed = 7f;
+
+    bool isJumping = false;
+
+    private void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        directionX = Input.GetAxisRaw("Horizontal") * moveSpeed;
+
+        Jump();
+    }
+
+    private void FixedUpdate()
+    {
+        rb.velocity = new Vector2(directionX, rb.velocity.y);
+    }
+
+    void Jump()
+    {
+        if (Input.GetButtonDown("Jump") && isJumping == false)
+        {
+            rb.AddForce(Vector2.up * jumpSpeed, ForceMode2D.Impulse);
+            isJumping = true;
+        }
+    }
+    
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if(collision.gameObject.tag == "Ground")
+        {
+            Debug.Log("Collides with deathzone");
+            isJumping = false;
+        }
+    }
+}
