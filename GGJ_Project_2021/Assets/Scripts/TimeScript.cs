31766ed9cc534893425873fbf9978f658bf2e32a
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-01-28 15:08:30 +0100

    Timer added

--- GGJ_Project_2021/Assets/Scripts/TimeScript.cs
+++ GGJ_Project_2021/Assets/Scripts/TimeScript.cs
@@ -1,18 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TimeScript : MonoBehaviour
 {
-    // Start is called before the first frame update
+    Image timerBar;
+    public float maxTime = 10f;
+    public float currentTime;
+    public GameObject timesUp;
+
     void Start()
     {
-        
+        timesUp.SetActive(false);
+        timerBar = GetComponent<Image>();
+        currentTime = maxTime;
     }
 
-    // Update is called once per frame
     void Update()
     {
-        
+        if(currentTime > 0)
+        {
+            currentTime -= Time.deltaTime;
+            timerBar.fillAmount = currentTime / maxTime;
+        }
+        else
+        {
+            timesUp.SetActive(true);
+            Time.timeScale = 0;
+        }
     }
 }
