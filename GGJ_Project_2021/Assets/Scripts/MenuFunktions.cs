690cd8ee406f7d59f80c92f6bcac90b282f42b16
DreamingJohnny <johnnydreaming@gmail.com>
2021-01-28 10:39:04 +0100

    Created prefabs and empty scripts

--- GGJ_Project_2021/Assets/Scripts/MenuFunktions.cs
+++ GGJ_Project_2021/Assets/Scripts/MenuFunktions.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class First_Script : MonoBehaviour
+public class MenuFunktions : MonoBehaviour
 {
     // Start is called before the first frame update
     void Start()
