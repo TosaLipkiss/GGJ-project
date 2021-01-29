4afd57dc00b0e034d5631becb101d566f5e1b0e7
TosaLipkiss <tovahl2002@skola.goteborg.se>
2021-01-29 11:58:54 +0100

    restartButton

---
+++ GGJ_Project_2021/Assets/Scripts/GUIButtons.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GUIButtons : MonoBehaviour
+{
+    public void restart()
+    {
+        Debug.Log("restarting");
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    }
+}
